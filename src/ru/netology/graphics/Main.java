72b19dad127524220f85fe543a90eb1a0478319f
Andrei <clinri@ya.ru>
2022-01-31 16:48:12 +0300

    Start course project.

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -14,8 +14,8 @@ public class Main {
         server.start(); // Запускаем
 
         // Или то же, но с выводом на экран:
-        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-        //String imgTxt = converter.convert(url);
-        //System.out.println(imgTxt);
+//        String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
     }
 }
